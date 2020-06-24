2d9156356c66ac64f53a57bd87ab212172cf1fc1
Daniel Alves <daniel.alves@knewin.com>
2020-06-23 23:26:06 -0300

    serverless function test 6

--- api/deploy.js
+++ api/deploy.js
@@ -1,14 +1,14 @@
 export default (req, res) => {
   const { secret, type, releases } = req.body
 
-  if (process.env.SECRET === secret) {
-    if (type === 'api-update') {
-      res.json({ releases })
-    }
-
-    res.status(403)
-    res.json({ message: `Invalid type ${type}` })
+  if (process.env.SECRET !== secret) {
+    return res.status(401)
   }
 
-  res.status(401)
+  if (type !== 'api-update') {
+    res.status(403)
+    return res.json({ message: `Invalid type ${type}` })
+  }
+
+  res.json({ releases })
 }
