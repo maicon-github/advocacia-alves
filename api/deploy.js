bf9d911c21e4305fe6e23a6cccbc08e207cadff4
Daniel Alves <daniel.alves@knewin.com>
2020-06-24 01:05:46 -0300

    serverless function test 7

--- api/deploy.js
+++ api/deploy.js
@@ -1,7 +1,9 @@
+import Bitbucket from 'bitbucket'
+
 export default (req, res) => {
   const { secret, type, releases } = req.body
 
-  if (process.env.SECRET !== secret) {
+  if (process.env.PRISMIC_SECRET !== secret) {
     return res.status(401)
   }
 
@@ -9,6 +11,25 @@ export default (req, res) => {
     res.status(403)
     return res.json({ message: `Invalid type ${type}` })
   }
+
+  const bitbucket = new Bitbucket({
+    auth: { username: process.env.GIT_USERNAME, password: process.env.GIT_PASSWORD }
+  })
+
+  // bitbucket.commits
+  //   .createApproval({ node: '', repo_slug: '', workspace: '' })
+  //   .then(({ data, headers, status, url }) => {})
+  //   .catch(({ message, error, headers, request, status }) => {})
+
+  bitbucket.commits
+    .createApproval({ repo_slug: process.env.GIT_REPOSITORY, workspace: process.env.GIT_WORKSPACE })
+    .then(({ data, headers, status, url }) => {
+      res.json({ data, headers, status, url })
+    })
+    .catch(({ message, error, headers, request, status }) => {
+      res.status(500)
+      res.json({ message, error, headers, request, status })
+    })
 
   res.json({ releases })
 }
