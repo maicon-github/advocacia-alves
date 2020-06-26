e79ff0f803a6e101f10459720568a81b1899b05e
Daniel Alves <daniel.alves@knewin.com>
2020-06-25 23:09:18 -0300

    feita integração com o zoho

--- api/lead.js
+++ api/lead.js
@@ -1,38 +1,20 @@
-import validator from 'validator'
-import xssFilters from 'xss-filters'
-import zcrmsdk from 'zcrmsdk'
+import zcrmsdk from '@trifoia/zcrmsdk'
 
 export default (req, res) => {
   const { name, email, phone } = req.body
 
-  const attributes = ['name', 'email', 'phone']
-
-  const sanitizedAttributes = attributes.map(n => validateAndSanitize(n, req.body[n]))
-
-  const someInvalid = sanitizedAttributes.some(r => !r)
-
-  if (someInvalid) {
-    return res.status(422).json({ message: 'Não foi possível agendar.', value: sanitizedAttributes })
-  }
-
   zcrmsdk.initialize({
-    client_id: '1000.GMS0S8RRJG7805AZ45KS5PW3WTP42H', // mandatory
-    client_secret: '05f226c697724129ca0c4f3bbf1fa9a03f8866a103', // mandatory
-    redirect_url: 'https://advocacia-alves.daniellv.vercel.app/'// mandatory
-  }).then(function () {
-    zcrmsdk.API.MODULES.post({
+    client_id: process.env.ZOHO_CLIENT_ID,
+    redirect_url: process.env.ZOHO_REDIRECT_URL,
+    client_secret: process.env.ZOHO_CLIENT_SECRET,
+    refresh_token: process.env.ZOHO_REFRESH_TOKEN,
+    user_identifier: process.env.ZOHO_USER_IDENTIFIER,
+    mysql_module: 'local'
+  }).then((client) => {
+    client.API.MODULES.post({
       module: 'Leads',
-      params: { name, email, phone }
-    })
+      body: { data: [{ Email: email, Last_Name: name, Mobile: phone }] }
+    }).then(data => res.status(200).json(JSON.parse(data.body)))
+      .catch(err => res.status(400).json(err))
   })
-}
-
-const rejectFunctions = new Map([
-  ['name', v => v.trim() === '' || v.length < 4],
-  ['phone', v => v.trim() === '' || v.length < 11],
-  ['email', v => v.trim() !== '' && !validator.isEmail(v)]
-])
-
-const validateAndSanitize = (key, value) => {
-  return rejectFunctions.has(key) && !rejectFunctions.get(key)(value) && xssFilters.inHTMLData(value)
 }
