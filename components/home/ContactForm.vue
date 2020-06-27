7f391515a04a7b6ac15c8c768c68dc5a7cdb2cc5
Daniel Alves <daniel.alves@knewin.com>
2020-06-27 16:09:55 -0300

    organizando fonte

--- components/home/ContactForm.vue
+++ components/home/ContactForm.vue
@@ -1,47 +1,47 @@
 <template>
   <div style="background-color: #003554" class="white--text pa-6 rounded-lg text-center">
     <p> Quero tirar minhas dúvidas com um advogado </p>
-    <v-form ref="leadForm" v-model="valid" lazy-validation>
+    <v-form ref="leadForm" v-model="form.valid" lazy-validation>
       <v-text-field
-        v-model="name"
+        v-model="form.name"
         label="Seu nome"
-        :rules="nameRules"
+        :rules="rules.name"
         background-color="white"
         filled
         dense
         required
       />
       <v-text-field
-        v-model="phone"
+        v-model="form.phone"
         v-mask="['(##) ####-####', '(##) #####-####']"
-        :rules="phoneRules"
+        :rules="rules.phone"
         background-color="white"
         label="Telefone (Whatsapp)"
         filled
         dense
         required
       />
-      <v-btn color="#e57100" class="my-auto" dark :loading="loading" @click="submit">
+      <v-btn color="#e57100" class="my-auto" dark :loading="form.submitting" @click="submit">
         <span class="white--text text-uppercase">
           Solicitar ligação
         </span>
       </v-btn>
       <v-snackbar
-        v-model="snackbar"
+        v-model="snackbar.show"
         :timeout="6000"
         centered
         absolute
-        color="success"
+        :color="snackbar.color"
         :top="true"
       >
-        {{ message }}
+        {{ snackbar.message }}
 
         <template v-slot:action="{ attrs }">
           <v-btn
             text
             color="blue"
             v-bind="attrs"
-            @click="snackbar = false"
+            @click="snackbar.show = false"
           >
             Fechar
           </v-btn>
@@ -53,42 +53,57 @@
 <script>
 export default {
   data: () => ({
-    message: '',
-    snackbar: false,
-    valid: true,
-    loading: false,
-    name: '',
-    nameRules: [
-      v => !!v || 'Preencha com seu nome.'
-    ],
-    phone: '',
-    phoneRules: [
-      v => !!v || 'Preencha com seu telefone celular.',
-      v => /(\(?\d{2}\)?\s)?(\d{4,5}-\d{4})/.test(v) || 'Preencha com seu telefone celular.'
-    ]
+    snackbar: { show: false, message: '', color: 'success' },
+    form: { name: '', phone: '', valid: true, submitting: false },
+    rules: {
+      name: [
+        v => !!v || 'Preencha com seu nome.'
+      ],
+      phone: [
+        v => !!v || 'Preencha com seu telefone celular.',
+        v => /(\(?\d{2}\)?\s)?(\d{4,5}-\d{4})/.test(v) || 'Preencha com seu telefone celular.'
+      ]
+    }
   }),
   methods: {
+    showErrorMessage () {
+      this.snackbar = {
+        show: false,
+        color: 'error',
+        message: 'Houve um erro ao agendar tente novamente mais tarde'
+      }
+    },
+    showSuccessMessage () {
+      this.snackbar = {
+        show: true,
+        color: 'success',
+        message: 'Solicitação enviada, em breve entraremos em contato pelo número informado'
+      }
+    },
+    resetFormValues () {
+      this.form = { name: '', phone: '', valid: true, submitting: false }
+      this.$refs.leadForm.resetValidation()
+    },
     submit () {
+      if (this.loading) { return }
+
       if (this.$refs.leadForm.validate()) {
-        this.loading = true
-        this.$axios.post(`${window.location.href}api/lead`, { name: this.name, phone: this.phone })
+        this.form.submitting = true
+        this.$axios.post(`${window.location.href}api/lead`, { name: this.form.name, phone: this.form.phone })
           .then((res) => {
             if (res.status === 200) {
-              window.console.log(res)
-              this.name = ''
-              this.phone = ''
-              this.message = 'Solicitação enviada, em breve entraremos em contato pelo número informado'
-            } else if ('message' in res.data) {
-              this.message = this.data.message
+              this.resetFormValues()
+              this.showSuccessMessage()
+            } else {
+              this.showErrorMessage()
             }
           })
           .catch((err) => {
             window.console.log(err)
-            this.message = 'Houve um erro ao agendar tente novamente mais tarde'
+            this.showErrorMessage()
           })
           .finally(() => {
-            this.loading = false
-            this.snackbar = true
+            this.form.submitting = false
           })
       }
     }
