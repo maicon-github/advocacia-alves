a36cdb273b5b5d4b698015bd5d990694d70aaea3
Daniel Alves <daniel.alves@knewin.com>
2020-07-31 23:47:43 -0300

    inserção de lead com recaptcha

--- api/lead.js
+++ api/lead.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default (req, res) => {
-  // const { name, email, phone } = req.body
+  const { name, email, phone } = req.body
 
   const config = {
     client_id: process.env.ZOHO_CLIENT_ID,
@@ -10,41 +10,25 @@ export default (req, res) => {
     refresh_token: process.env.ZOHO_REFRESH_TOKEN,
     iamurl: 'accounts.zoho.com',
     base_url: 'www.zohoapis.com',
-    user_identifier: process.env.ZOHO_USER_IDENTIFIER,
+    user_identifier: process.env.ZOHO_USER_IDENTIFIER
   }
 
   axios.post(`https://accounts.zoho.com/oauth/v2/token?refresh_token=${config.refresh_token}&client_id=${config.client_id}&client_secret=${config.client_secret}&grant_type=refresh_token`)
-    .then((json) => {
-      res.status(200).send(json)
+    .then((auth) => {
+      axios({
+        method: 'post',
+        url: 'https://www.zohoapis.com/crm/v2/Leads',
+        headers: { authorization: `Zoho-oauthtoken ${auth.data.access_token}` },
+        data: { data: [{ Email: email, Last_Name: name, Phone: phone.replace(/\D/g, '') }] }
+      })
+        .then((lead) => {
+          res.status(200).json(lead.data)
+        })
+        .catch((err) => {
+          res.status(400).json(err)
+        })
     })
-    .catch((error) => {
-      res.send(error)
+    .catch((err) => {
+      res.status(400).json(err)
     })
-
-  // let headers = {
-  //   Authorization: 'Zoho-oauthtoken ' + config.accessToken,
-  //   'Content-Type': 'application/json'
-  // }
-
-  // request.post({url, body:lead, headers, json: true}, function (err, response, body) {
-  //     console.log("Zoho crm response", err, body);
-  //     if (err) {
-  //         console.log("Failed to add as lead in Zoho CRM. Error: ", err);
-  //     }
-  //     console.log("TESTING ZOHO API V2",body.data[0].details);              
-  // });
-  // zcrmsdk.initialize({
-  //   client_id: process.env.ZOHO_CLIENT_ID,
-  //   redirect_url: process.env.ZOHO_REDIRECT_URL,
-  //   client_secret: process.env.ZOHO_CLIENT_SECRET,
-  //   refresh_token: process.env.ZOHO_REFRESH_TOKEN,
-  //   user_identifier: process.env.ZOHO_USER_IDENTIFIER,
-  //   mysql_module: 'local'
-  // }).then((client) => {
-  //   client.API.MODULES.post({
-  //     module: 'Leads',
-  //     body: { data: [{ Email: email, Last_Name: name, Phone: phone.replace(/\D/g, '') }] }
-  //   }).then(data => res.status(200).json(JSON.parse(data.body)))
-  //     .catch(err => res.status(400).json(err))
-  // })
 }
