5acf70136c7d5ee5559d9a12ba1fa8fb5b8d49b7
Daniel Alves <daniel.alves@knewin.com>
2020-12-15 00:40:25 -0300

    bugs visuais no header

--- components/shared/Header.vue
+++ components/shared/Header.vue
@@ -2,57 +2,30 @@
   <v-app-bar fixed app>
     <v-container class="d-flex py-0">
       <v-app-bar-nav-icon class="hidden-md-and-up my-auto ml-n6" @click="$emit('menu')" />
-      <a href="/" :class="logoClass">
+      <a href="/" class="ma-auto">
         <img height="52" src="/logo.png" alt="Advocacia Alves">
       </a>
       <v-spacer class="hidden-sm-and-down" />
-      <div class="my-auto pr-4 hidden-sm-and-down">
+      <div class="d-flex my-auto pr-4 hidden-sm-and-down">
         <v-btn href="/" class="my-auto" text small>
           Inicio
         </v-btn>
         <v-btn href="/sobre" class="my-auto" text small>
           Sobre
         </v-btn>
-        <v-menu open-on-hover offset-y>
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn
-              href="/blog"
-              class="my-auto"
-              v-bind="attrs"
-              small
-              text
-              v-on="on"
-            >
-              Blog
-              <v-icon dense>
-                mdi-menu-down
-              </v-icon>
-            </v-btn>
-          </template>
-          <v-list dense>
-            <v-list-item key="1" href="/blog/categoria/aposentadoria">
-              <v-list-item-title>Aposentadoria</v-list-item-title>
-            </v-list-item>
-            <v-list-item key="2" href="/blog/categoria/auxilio">
-              <v-list-item-title>Auxílio</v-list-item-title>
-            </v-list-item>
-            <v-list-item key="3" href="/blog/categoria/bpc-loas">
-              <v-list-item-title>BPC - LOAS</v-list-item-title>
-            </v-list-item>
-            <v-list-item key="4" href="/blog/categoria/dica">
-              <v-list-item-title>Dica</v-list-item-title>
-            </v-list-item>
-            <v-list-item key="5" href="/blog/categoria/revisao">
-              <v-list-item-title>Revisão</v-list-item-title>
-            </v-list-item>
-            <v-list-item key="6" href="/blog/categoria/salario-maternidade">
-              <v-list-item-title>Salário maternidade</v-list-item-title>
-            </v-list-item>
-            <v-list-item key="7" href="/blog/categoria/pensao">
-              <v-list-item-title>Pensão</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
+        <v-btn
+          href="/blog"
+          class="my-auto"
+          :style="tempBlogStyle"
+          small
+          text
+        >
+          Blog
+          <v-icon dense>
+            mdi-menu-down
+          </v-icon>
+        </v-btn>
+        <BlogMenu @mounted="hideBlogTemp()" />
         <v-btn href="/central-de-ajuda" class="my-auto" text small>
           Central de ajuda
         </v-btn>
@@ -70,40 +43,49 @@
           Consulta à Distância
         </v-btn>
       </div>
-      <v-menu offset-y bottom left :close-on-content-click="false" :close-on-click="false">
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            icon
-            :class="searchClass"
-            v-bind="attrs"
-            v-on="on"
-          >
-            <v-icon>
-              mdi-magnify
-            </v-icon>
-          </v-btn>
-        </template>
-        <v-card class="pa-4 pb-2" width="300px">
-          <v-text-field label="Pesquisar no blog" dense hide-details autofocus />
-        </v-card>
-      </v-menu>
+      <v-btn icon :style="tempSearchStyle" class="my-auto">
+        <v-icon>
+          mdi-magnify
+        </v-icon>
+      </v-btn>
+      <SearchMenu @mounted="hideSearchTemp" />
     </v-container>
   </v-app-bar>
 </template>
 <script>
+import BlogMenu from '../blog/BlogMenu'
+import SearchMenu from '../shared/SearchMenu'
 export default {
+  components: { BlogMenu, SearchMenu },
+  data () {
+    return {
+      showBlogTemp: true,
+      showSearchTemp: true
+    }
+  },
   computed: {
-    searchClass () {
-      if (this.$vuetify.breakpoint.smAndDown) {
-        return 'mr-n6 my-auto'
-      }
-      return 'my-auto'
+    tempBlogStyle () {
+      return this.showBlogTemp ? '' : 'display:none;'
+    },
+    tempSearchStyle () {
+      return this.showSearchTemp ? '' : 'display:none;'
     },
     logoClass () {
       if (this.$vuetify.breakpoint.smAndDown) {
         return 'ma-auto'
       }
       return 'ma-auto ml-n6'
+    },
+    data () {
+      return { showTemp: true }
+    }
+  },
+  methods: {
+    hideBlogTemp () {
+      this.showBlogTemp = false
+    },
+    hideSearchTemp () {
+      this.showSearchTemp = false
     }
   }
 }
---
+++ components/shared/SearchMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-menu offset-y bottom left :close-on-content-click="false" :close-on-click="false">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        icon
+        class="my-auto"
+        v-bind="attrs"
+        v-on="on"
+      >
+        <v-icon>
+          mdi-magnify
+        </v-icon>
+      </v-btn>
+    </template>
+    <v-card class="pa-4 pb-2" width="300px">
+      <v-text-field label="Pesquisar no blog" dense hide-details autofocus />
+    </v-card>
+  </v-menu>
+</template>
+<script>
+export default {
+  mounted () {
+    this.$emit('mounted')
+  }
+}
+</script>
