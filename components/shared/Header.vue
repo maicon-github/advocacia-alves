d7e8c2c4cdacfe44a42710a595697c875e552c04
Daniel Alves <daniel.alves@knewin.com>
2020-12-28 09:28:10 -0300

    correção navigation

--- components/shared/Header.vue
+++ components/shared/Header.vue
@@ -6,42 +6,44 @@
         <img height="52" src="/logo.png" alt="Advocacia Alves">
       </a>
       <v-spacer class="hidden-sm-and-down" />
-      <div class="d-flex hidden-sm-and-down my-auto pr-4">
-        <v-btn href="/" class="my-auto" text small>
-          Inicio
-        </v-btn>
-        <v-btn href="/sobre" class="my-auto" text small>
-          Sobre
-        </v-btn>
-        <v-btn
-          href="/blog"
-          class="my-auto"
-          :style="tempBlogStyle"
-          small
-          text
-        >
-          Blog
-          <v-icon dense>
-            mdi-menu-down
-          </v-icon>
-        </v-btn>
-        <BlogMenu @mounted="hideBlogTemp()" />
-        <v-btn href="/central-de-ajuda" class="my-auto" text small>
-          Central de ajuda
-        </v-btn>
-        <v-btn
-          href="https://advocaciaalves.zohorecruit.com/jobs/carreira"
-          class="my-auto"
-          target="_blank"
-          rel="noreferrer"
-          text
-          small
-        >
-          Carreira
-        </v-btn>
-        <v-btn to="/consulta-a-distancia" class="my-auto white--text" color="#e57100" small>
-          Consulta à Distância
-        </v-btn>
+      <div class="hidden-sm-and-down my-auto">
+        <div class="d-flex pr-4">
+          <v-btn href="/" class="my-auto" text small>
+            Inicio
+          </v-btn>
+          <v-btn href="/sobre" class="my-auto" text small>
+            Sobre
+          </v-btn>
+          <v-btn
+            href="/blog"
+            class="my-auto"
+            :style="tempBlogStyle"
+            small
+            text
+          >
+            Blog
+            <v-icon dense>
+              mdi-menu-down
+            </v-icon>
+          </v-btn>
+          <BlogMenu @mounted="hideBlogTemp()" />
+          <v-btn href="/central-de-ajuda" class="my-auto" text small>
+            Central de ajuda
+          </v-btn>
+          <v-btn
+            href="https://advocaciaalves.zohorecruit.com/jobs/carreira"
+            class="my-auto"
+            target="_blank"
+            rel="noreferrer"
+            text
+            small
+          >
+            Carreira
+          </v-btn>
+          <v-btn to="/consulta-a-distancia" class="my-auto white--text" color="#e57100" small>
+            Consulta à Distância
+          </v-btn>
+        </div>
       </div>
       <v-btn icon :style="tempSearchStyle" class="my-auto">
         <v-icon>
