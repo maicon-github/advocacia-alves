130c54b0f1cf11268789f6dedfc21c2716a52be1
Daniel Alves <daniel.alves@knewin.com>
2021-01-02 10:10:16 -0300

    teste com preload e async

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,6 @@ export default {
       { 'http-equiv': 'cache-control', content: 'max-age=31536000' }
     ],
     link: [
-      { rel: 'preload', href: 'https://cdn.jsdelivr.net/npm/@mdi/font@latest/fonts/materialdesignicons-webfont.woff2?v=5.8.55', as: 'font', crossorigin: true },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'preconnect', href: 'https://images.prismic.io' },
       { rel: 'dns-prefetch', href: 'https://images.prismic.io' },
@@ -31,7 +30,10 @@ export default {
       { rel: 'preconnect', href: 'https://prismic-io.s3.amazonaws.com' },
       { rel: 'dns-prefetch', href: 'https://prismic-io.s3.amazonaws.com' },
       { rel: 'preconnect', href: 'https://www.gstatic.com' },
-      { rel: 'dns-prefetch', href: 'https://www.gstatic.com' }
+      { rel: 'dns-prefetch', href: 'https://www.gstatic.com' },
+      { rel: 'preload', as: 'font', href: 'https://cdn.jsdelivr.net/npm/@mdi/font@latest/fonts/materialdesignicons-webfont.woff2?v=5.8.55', crossorigin: '' },
+      { rel: 'stylesheet', async: true, href: 'https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css' },
+      { rel: 'stylesheet', async: true, href: 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900&display=swap' }
     ]
   },
   /*
