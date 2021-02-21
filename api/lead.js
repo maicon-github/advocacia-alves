516a121f3373e68b473720de1453f337b856c484
Daniel Alves <dnl.alv@gmail.com>
2021-02-21 01:23:44 -0300

    security

--- api/lead.js
+++ api/lead.js
@@ -1,34 +1,36 @@
 import axios from 'axios'
 
 export default (req, res) => {
-  const { name, email, phone } = req.body
+  if (req.headers.host == "advocaciaalves.com.br") {
+    const { name, email, phone } = req.body
 
-  const config = {
-    client_id: process.env.ZOHO_CLIENT_ID,
-    client_secret: process.env.ZOHO_CLIENT_SECRET,
-    redirect_url: process.env.ZOHO_REDIRECT_URL,
-    refresh_token: process.env.ZOHO_REFRESH_TOKEN,
-    iamurl: 'accounts.zoho.com',
-    base_url: 'www.zohoapis.com',
-    user_identifier: process.env.ZOHO_USER_IDENTIFIER
-  }
+    const config = {
+      client_id: process.env.ZOHO_CLIENT_ID,
+      client_secret: process.env.ZOHO_CLIENT_SECRET,
+      redirect_url: process.env.ZOHO_REDIRECT_URL,
+      refresh_token: process.env.ZOHO_REFRESH_TOKEN,
+      iamurl: 'accounts.zoho.com',
+      base_url: 'www.zohoapis.com',
+      user_identifier: process.env.ZOHO_USER_IDENTIFIER
+    }
 
-  axios.post(`https://accounts.zoho.com/oauth/v2/token?refresh_token=${config.refresh_token}&client_id=${config.client_id}&client_secret=${config.client_secret}&grant_type=refresh_token`)
-    .then((auth) => {
-      axios({
-        method: 'post',
-        url: 'https://www.zohoapis.com/crm/v2/Leads',
-        headers: { authorization: `Zoho-oauthtoken ${auth.data.access_token}` },
-        data: { data: [{ Email: email, Last_Name: name, Phone: phone.replace(/\D/g, '') }] }
+    axios.post(`https://accounts.zoho.com/oauth/v2/token?refresh_token=${config.refresh_token}&client_id=${config.client_id}&client_secret=${config.client_secret}&grant_type=refresh_token`)
+      .then((auth) => {
+        axios({
+          method: 'post',
+          url: 'https://www.zohoapis.com/crm/v2/Leads',
+          headers: { authorization: `Zoho-oauthtoken ${auth.data.access_token}` },
+          data: { data: [{ Email: email, Last_Name: name, Phone: phone.replace(/\D/g, '') }] }
+        })
+          .then((lead) => {
+            res.status(200).json(lead.data)
+          })
+          .catch((err) => {
+            res.status(400).json(err)
+          })
       })
-        .then((lead) => {
-          res.status(200).json(lead.data)
-        })
-        .catch((err) => {
-          res.status(400).json(err)
-        })
-    })
-    .catch((err) => {
-      res.status(400).json(err)
-    })
+      .catch((err) => {
+        res.status(400).json(err)
+      })
+  }
 }
