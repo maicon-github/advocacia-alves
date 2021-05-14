151e467fba151dc4e5a26fc51cc647a0f7273591
Daniel Alves <daniel.alves@way2.com.br>
2021-05-13 22:32:21 -0300

    criando questionário de atendimento a partir de configurações no prismic

--- pages/atendimento-inicial.vue
+++ pages/atendimento-inicial.vue
@@ -1,5 +1,6 @@
 <template>
   <FlowForm
+    v-if="questions.length>0"
     :questions="questions"
     :language="language"
     :standalone="true"
@@ -7,30 +8,122 @@
   />
 </template>
 <script>
-import FlowForm, { LanguageModel } from '@ditdot-dev/vue-flow-form'
-// import contact from '../components/quizzes/contact'
-// import aposentadoria from '../components/quizzes/aposentadoria'
-// import revisao from '../components/quizzes/revisao'
-import incapacidade from '../components/quizzes/incapacidade'
+import FlowForm, { LanguageModel, QuestionModel, QuestionType, ChoiceOption } from '@ditdot-dev/vue-flow-form'
 export default {
   layout: 'blank',
   components: { FlowForm },
+  async asyncData ({ $prismic, error }) {
+    try {
+      const page = (await $prismic.api.getSingle('attendance'))
+
+      return { page }
+    } catch (e) {
+      error({ statusCode: 500, title: 'Internal Server Error' })
+    }
+  },
   data () {
     return {
-      current: null,
-      questions: [
-        // ...contact,
-        // ...aposentadoria,
-        // ...revisao,
-        ...incapacidade
-      ],
+      questions: [],
       language: new LanguageModel({
         continue: 'Continuar',
         pressEnter: 'Pressione ENTER'
       })
     }
   },
+  mounted () {
+    window.console.log(this.page)
+    const config = this.page.data.body
+
+    const questions = []
+    for (const i in config) {
+      const { primary, items } = config[i]
+      if (config[i].slice_type === 'section_break') {
+        questions.push(this.createSectionBreak(primary))
+      }
+      if (config[i].slice_type === 'text') {
+        questions.push(this.createTextField(primary))
+      }
+      if (config[i].slice_type === 'phone') {
+        questions.push(this.createPhoneField(primary))
+      }
+      if (config[i].slice_type === 'e-mail') {
+        questions.push(this.createEmailField(primary))
+      }
+      if (config[i].slice_type === 'select_field') {
+        questions.push(this.createSelectField(primary, items))
+      }
+    }
+
+    this.questions = questions
+  },
   methods: {
+    createSectionBreak (data) {
+      return new QuestionModel({
+        id: data.id,
+        title: data.title,
+        description: data.description,
+        content: data.content,
+        type: QuestionType.SectionBreak,
+        jump: () => data.jump
+      })
+    },
+    createTextField (data) {
+      return new QuestionModel({
+        id: data.id,
+        title: data.question,
+        type: QuestionType.Text,
+        required: data.required,
+        mask: data.mask,
+        placeholder: data.placeholder,
+        jump: () => data.jump
+      })
+    },
+    createPhoneField (data) {
+      return new QuestionModel({
+        id: data.id,
+        title: data.question,
+        type: QuestionType.Phone,
+        placeholder: data.placeholder,
+        required: data.required,
+        jump: () => data.jump
+      })
+    },
+    createEmailField (data) {
+      window.console.log(data)
+      return new QuestionModel({
+        id: data.id,
+        title: data.question,
+        type: QuestionType.Email,
+        placeholder: data.placeholder,
+        required: data.required,
+        jump: () => data.jump
+      })
+    },
+    createSelectField (data, items) {
+      const { options, jump } = this.createChoiceOptions(items)
+
+      return new QuestionModel({
+        id: data.id,
+        title: data.question,
+        type: QuestionType.MultipleChoice,
+        helpText: data.help_text,
+        required: data.required,
+        multiple: data.multiple,
+        options,
+        jump
+      })
+    },
+    createChoiceOptions (items) {
+      const options = []
+      const jump = {}
+      for (const i in items) {
+        options.push(new ChoiceOption({ label: items[i].label, value: items[i].value }))
+        jump[items[i].value] = items[i].jump
+      }
+
+      return { options, jump }
+    }
+
     // step (id, question) {
     //   window.console.log(id)
     //   if (id === 'AdvogadoCuidando') {
